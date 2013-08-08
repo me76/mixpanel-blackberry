4912afe7cfd07b2dd0598cb9c5884aa7a3b48a56
Joe Bowers <joe@mixpanel.com>
2013-08-08 16:05:10 -0700

    Version bump

--- Mixpanel.cpp
+++ Mixpanel.cpp
@@ -34,7 +34,7 @@ static initialization init;
 details::MessageThread Mixpanel::s_thread;
 details::Preferences Mixpanel::s_preferences;
 
-const char Mixpanel::VERSION[] = "0.0.1pre";
+const char Mixpanel::VERSION[] = "0.0.2pre";
 
 Mixpanel::Mixpanel(const QString &token)
    : m_token(token) {
